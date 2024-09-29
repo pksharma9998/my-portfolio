b4c5f8f665572e6ad8e45b41809234a57ccd7118
Pawan Sharma <pawansharma9998@gmail.com>
2024-09-29 17:19:02 +0530

    Feat: projects details

--- src/config/index.js
+++ src/config/index.js
@@ -1,54 +1,68 @@
 export const CONFIG = {
-    PS: 'PS',
-    NAME: 'PAWAN SHARMA',
-    LOWER_NAME: 'Pawan Sharma',
-    LOCATION: 'Surat, Gujarat.',
-    ABOUT1: 'I am currently employed as a software developer at Elitist Software Solutions LLP.',
-    ABOUT2: 'I have completed Master of Computer Applications at Vidyabharti Trust College of MCA, affiliated with Gujarat Technological University (GTU).',
-    ABOUT3: 'Apart from coding, some other activities that I love to do!'
+  PS: 'PS',
+  NAME: 'PAWAN SHARMA',
+  LOWER_NAME: 'Pawan Sharma',
+  LOCATION: 'Surat, Gujarat.',
+  ABOUT1: 'I am currently employed as a software developer at Elitist Software Solutions LLP.',
+  ABOUT2: 'I have completed Master of Computer Applications at Vidyabharti Trust College of MCA, affiliated with Gujarat Technological University (GTU).',
+  ABOUT3: 'Apart from coding, some other activities that I love to do!'
 };
 
 export const PROJECTS = [
-    {
-      imgPath: "",
-      title: "QUIRKPLUS",
-      description: "QuirkPlus is a comprehensive daily planner app designed to help users manage their tasks efficiently while enhancing productivity. I’ve worked in admin model in which users, tasks, reports, hosts are managed. And I’ve designed the application.",
-      ghLink: "",
-      demoLink: "https://quirkplus.com/"
-    },
-    {
-      imgPath: "",
-      title: "REAL MOTORS",
-      description: "This is a admin panel of the Real Motors on which I’ve work so this project have 3 modules. 2 is applications both are customer application and 1 is web application that is vendor application. I’ve work on admin model so all the types of requests and information comes to here. There all things are based on car services.",
-      ghLink: "",
-      demoLink: ""
-    },
-    {
-      imgPath: "",
-      title: "3elitist.com",
-      description: "Elitist Software Solutions LLP is a global IT solution provider company in surat with +6 years of competitive business experience in niche industries.",
-      ghLink: "",
-      demoLink: "https://3elitist.com/"
-    },
-    {
-      imgPath: "",
-      title: "CROME",
-      description: "A Crypto Currency Project where client can place orders of crypto coins and tokens. Fully dynamic step by step requirement gathering using questions related to Coins and Tokens.",
-      ghLink: "",
-      demoLink: ""
-    },
-    {
-      imgPath: "",
-      title: "SOP (Standard Operating Procedure)",
-      description: "Simple organization’s internal management system. Admin can manage the Department And it’s Employees. Admin creates standard operating procedures for employees department wise.",
-      ghLink: "",
-      demoLink: ""
-    },
-    {
-      imgPath: "",
-      title: "Company UI",
-      description: "Changed the entire UI of the company's website in the next JS language with multiple languages.",
-      ghLink: "",
-      demoLink: "https://www.raininfotech.com/"
-    },
+  {
+    imgPath: "/assets/projects/quirkplus.png",
+    title: "QUIRKPLUS",
+    description: "QuirkPlus is a comprehensive daily planner app designed to help users manage their tasks efficiently while enhancing productivity. I’ve worked in admin model in which users, tasks, reports, hosts are managed. And I’ve designed the application.",
+    ghLink: "",
+    demoLink: "https://quirkplus.com/"
+  },
+  {
+    imgPath: "/assets/projects/real-motors.png",
+    title: "REAL MOTORS",
+    description: "This is a admin panel of the Real Motors on which I’ve work so this project have 3 modules. 2 is applications both are customer application and 1 is web application that is vendor application. I’ve work on admin model so all the types of requests and information comes to here. There all things are based on car services.",
+    ghLink: "",
+    demoLink: "https://play.google.com/store/apps/details?id=com.realmotor.itf&pcampaignid=web_share"
+  },
+  {
+    imgPath: "/assets/projects/elitist.png",
+    title: "3elitist.com",
+    description: "Elitist Software Solutions LLP is a global IT solution provider company in surat with +6 years of competitive business experience in niche industries.",
+    ghLink: "",
+    demoLink: "https://3elitist.com/"
+  },
+  {
+    imgPath: "/assets/projects/billwithus.png",
+    title: "Bill With Us",
+    description: "Bill With Us is about a billing system that is designed for billing experts, doctors, lawyers, software developers, and the marketing team.. It has no limits or restrictions.",
+    ghLink: "",
+    demoLink: "https://billwithus.com/"
+  },
+  {
+    imgPath: "/assets/projects/rain-infotech.png",
+    title: "Company UI",
+    description: "Changed the entire UI of the company's website in the next JS language.",
+    ghLink: "",
+    demoLink: "https://www.raininfotech.com/"
+  },
+  {
+    imgPath: "/assets/projects/3elixir.png",
+    title: "3elixir.com",
+    description: "3Elixir Solutions is a global IT solution provider company in surat with +6 years of competitive business experience in niche industries. We offer custom development backend development enterprise services and CMS website development.",
+    ghLink: "",
+    demoLink: "https://3elixir.com/"
+  },
+  {
+    imgPath: "",
+    title: "CROME",
+    description: "A Crypto Currency Project where client can place orders of crypto coins and tokens. Fully dynamic step by step requirement gathering using questions related to Coins and Tokens.",
+    ghLink: "",
+    demoLink: ""
+  }, 
+  {
+    imgPath: "",
+    title: "SOP (Standard Operating Procedure)",
+    description: "Simple organization’s internal management system. Admin can manage the Department And it’s Employees. Admin creates standard operating procedures for employees department wise.",
+    ghLink: "",
+    demoLink: ""
+  },
 ];
